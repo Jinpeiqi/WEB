39a6a289005529581f664000f1e81ec38d372404
Jinpeiqi <626335649@qq.com>
2016-03-28 19:09:55 +1300

    test

--- Test/untitled/js/main.js
+++ Test/untitled/js/main.js
@@ -1,104 +1,56 @@
-var myApp = angular
-    .module("myModule",[])
-    .filter("gender",function(){
-        return function (gender) {
-            switch (gender){
-                case "male" :
-                    return 1;
-                case "female":
-                    return 2;
-            }
-        }})
-    .controller("simplecontroller", function simplecontroller($scope){
-        $scope.customers = [
-            { name:"Dave Jones", city:"Phoenix"},
-            { name:"Jamie Riley", city:"Atlanta"},
-            { name:"Jin Peiqi", city:"NanJing"},
-            {name:"Heedy Wahlin", city:"Chandler"}
-        ]
-    })
-    .controller("myController",function($scope){
-        var a="JinPeiqi";
-        var employee = {
-            firstName:a,
-            lastName:"Hastings",
-            gender : "Male"
-        };
-        $scope.employee =  employee;
-        var picture = {
-            Name : "Jiang",
-            Year : "1989",
-            flag :"scr/pic/1.jpg"
-        };
-        $scope.picture = picture;
-        var message="Hello Angular";
-        $scope.message = message;
-        var countries =
-            [
-                {
-                    name :"UK",
-                    cities : [
-                        {name:"London"},
-                        {name:"Mancheste"},
-                        {name:"Birmingham"}
-                    ]
-                },
-                {
-                    name:"USA",
-                    cities:[
-                        {name:"Los Angeles"},
-                        {name:"Chicage"},
-                        {name:"Houston"}
-                    ]
-                },
-                {
-                    name: "India",
-                    cities: [
-                        {name: "Hyderabad"},
-                        {name: "Chennai"},
-                        {name: "Mumbai"}
-                    ]
-                }
-            ];
-        $scope.countries=countries;
+var app = angular.module('myapp', ['UserValidation']);
 
-        var technologies =[
-            { name:"C#",like:0,dislike:0},
-            { name:"C++",like:0,dislike:0},
-            { name:"JAVA",like:0,dislike:0},
-            { name:"JAVASCRIPT",like:0,dislike:0}
-        ];
-        $scope.technologies=technologies;
-        $scope.Like=function(technologies){
-            technologies.like++;
-        };
-        $scope.Dislike=function(technologies){
-            technologies.dislike++;
-        };
+myappCtrl = function($scope) {
+    $scope.formAllGood = function () {
+        return ($scope.usernameGood && $scope.passwordGood && $scope.passwordCGood)
+    }
 
-        var salary =[
-            {name:"jinpeiqi",gender:"male",salary:"5000"},
-            {name:"jinpeiqi1",gender:"male",salary:"2000"},
-            {name:"jinpeiqi2",gender:"male",salary:"3000"},
-            {name:"jinpeiqi3",gender:"male",salary:"4000"}
-        ];
-        $scope.salary=salary;
-        $scope.sortcolumn="name";
-        $scope.reverseSort=false;
-        $scope.sortData=function(column){
-            if($scope.sortcolumn=="column"){
-                $scope.reverseSort=true;
-            }else {
-                $scope.reverseSort=false;
-            }
-            $scope.sortcolumn=column;
-        };
-        var music={
-            name:"1",
-            scr:"scr/media/1.mp3"
-        };
-        $scope.music=music;
+}
 
-    });
+angular.module('UserValidation', []).directive('validUsername', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, elm, attrs, ctrl) {
+            ctrl.$parsers.unshift(function (viewValue) {
+                // Any way to read the results of a "required" angular validator here?
+                var isBlank = viewValue === ''
+                var invalidChars = !isBlank && !/^[A-z0-9]+$/.test(viewValue)
+                var invalidLen = !isBlank && !invalidChars && (viewValue.length < 5 || viewValue.length > 20)
+                ctrl.$setValidity('isBlank', !isBlank)
+                ctrl.$setValidity('invalidChars', !invalidChars)
+                ctrl.$setValidity('invalidLen', !invalidLen)
+                scope.usernameGood = !isBlank && !invalidChars && !invalidLen
 
+            })
+        }
+    }
+}).directive('validPassword', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, elm, attrs, ctrl) {
+            ctrl.$parsers.unshift(function (viewValue) {
+                var isBlank = viewValue === ''
+                var invalidLen = !isBlank && (viewValue.length < 8 || viewValue.length > 20)
+                var isWeak = !isBlank && !invalidLen && !/(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z])/.test(viewValue)
+                ctrl.$setValidity('isBlank', !isBlank)
+                ctrl.$setValidity('isWeak', !isWeak)
+                ctrl.$setValidity('invalidLen', !invalidLen)
+                scope.passwordGood = !isBlank && !isWeak && !invalidLen
 
+            })
+        }
+    }
+}).directive('validPasswordC', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, elm, attrs, ctrl) {
+            ctrl.$parsers.unshift(function (viewValue, $scope) {
+                var isBlank = viewValue === ''
+                var noMatch = viewValue != scope.myform.password.$viewValue
+                ctrl.$setValidity('isBlank', !isBlank)
+                ctrl.$setValidity('noMatch', !noMatch)
+                scope.passwordCGood = !isBlank && !noMatch
+            })
+        }
+    }
+})
